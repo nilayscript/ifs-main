adc966a249af9e49f4e5afd634e4a995180d045e
nilayscript <nilay.singh@digisigns.in>
2025-07-07 20:42:10 +0530

    Update

--- netlify/functions/get-image.js
+++ netlify/functions/get-image.js
@@ -12,10 +12,14 @@ exports.handler = async (event, context) => {
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+      body: "Preflight OK",
     };
   }
-
   try {
     // Get image path and token from query parameters
     const { path, token } = event.queryStringParameters || {};
--- netlify/functions/get-kpi-data.js
+++ netlify/functions/get-kpi-data.js
@@ -7,8 +7,12 @@ exports.handler = async (event, context) => {
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers,
-      body: JSON.stringify({ message: "CORS preflight success" }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+      body: "Preflight OK",
     };
   }
 
